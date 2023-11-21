a6781b757dbfff4d4722b36a989eab4e608460d7
vimmoos@Thor <vimmoos@Thor>
2023-11-21 23:53:05 +0100

    ohmyzsh

--- init.py
+++ init.py
@@ -83,22 +83,22 @@ def run(string, *args):
 
 c = Colorcodes()
 
-c.info("===========Starting Post installation===========")
-c.info("Creating default folders")
-try:
-    for v in FOLDERS:
-        run(f"mkdir {v}")
-except Exception as e:
-    c.error(f"ERROR:\n{e}")
+# c.info("===========Starting Post installation===========")
+# c.info("Creating default folders")
+# try:
+#     for v in FOLDERS:
+#         run(f"mkdir {v}")
+# except Exception as e:
+#     c.error(f"ERROR:\n{e}")
 
-c.complete("DONE creating folders")
+# c.complete("DONE creating folders")
 
-c.info("START installing utils")
-try:
-    run(f"sudo pacman -Syyu --noconfirm {' '.join(UTILS)}")
-except Exception as e:
-    c.error(f"ERROR:\n{e}")
-c.complete("DONE installing utils")
+# c.info("START installing utils")
+# try:
+#     run(f"sudo pacman -Syyu --noconfirm {' '.join(UTILS)}")
+# except Exception as e:
+#     c.error(f"ERROR:\n{e}")
+# c.complete("DONE installing utils")
 
 c.info("Set up zsh")
 try:
@@ -113,10 +113,10 @@ except Exception as e:
 
 c.complete("DONE with zsh")
 
-c.info("Copying all default configs")
-try:
-    for k, v in CONFIGS.items():
-        run(f"cp -r {HOME}/init_sys/{k} {v}")
-except Exception as e:
-    c.error(f"ERROR:\n{e}")
-c.complete("DONE with configs")
+# c.info("Copying all default configs")
+# try:
+#     for k, v in CONFIGS.items():
+#         run(f"cp -r {HOME}/init_sys/{k} {v}")
+# except Exception as e:
+#     c.error(f"ERROR:\n{e}")
+# c.complete("DONE with configs")
